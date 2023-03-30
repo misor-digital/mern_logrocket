622b8e464c3983a3c2a2779519c6e7dd1d26e764
misor.digital <misor.digital@gmail.com>
2023-03-30 16:28:51 +0300

    server init

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app     = express();
+const port    = process.env.PORT || 8082;
+
+app.get('/', (req, res) => res.send('Hello'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
