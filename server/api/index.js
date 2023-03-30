b27dc97235cf6beb539dcf1cfda319afd4ae6902
misor.digital <misor.digital@gmail.com>
2023-03-31 01:35:35 +0300

    server: api => model, router

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const router  = require('express').Router();
+const routers = require('./routers');
 
 // Load book model
 const Book = require('../models/Book');
