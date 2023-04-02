91e05a91f045ce5e5c8f8b060a985dd18c8932de
misor.digital <misor.digital@gmail.com>
2023-04-02 13:03:27 +0300

    [server] cors integration

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors    = require('cors');
 const cp      = require('cookie-parser');
 
 const config = require('./config');
@@ -6,8 +7,13 @@ const api    = require('./api');
 
 const app = express();
 
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json({ extended: false }));
 app.use(cp());
+
+// Routes
 app.use('/api', api);
+
 app.get('/', (req, res) => res.send('Hello'));
 
 app.listen(config.port, () => console.log(`Server@port: ${config.port}`));
