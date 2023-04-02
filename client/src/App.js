580aa15e74febbf2e957143174b93d4c448fede9
misor.digital <misor.digital@gmail.com>
2023-04-02 10:16:41 +0300

    [client] components: CreateBook => state, change;

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,6 @@ const App = () => {
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- client/src/components/CreateBook.js
+++ client/src/components/CreateBook.js
@@ -1,1 +1,24 @@
+import React, { useState }   from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios                 from axios;
 
+const CreateBook = (props) => {
+  // Define the state with useState hook
+  const navigate          = useNavigate();
+  const [ book, setBook ] = useState({
+    title          : '',
+    isbn           : '',
+    author         : '',
+    description    : '',
+    published_date : '',
+    publisher      : '',
+  });
+
+  const onChange = (e) => {
+    setBook({ ...book, [e.target.name] : e.target.value });
+  };
+
+  const onSubmit = (e) => {};
+
+  return null;
+};
